dd8282b2eac94f0698ad3a9dbfbfa9c09e515d5c
Gowtham Ramarajan <84629235+GowthamRam96@users.noreply.github.com>
2021-08-20 21:52:43 +0530

    Update LaplaceTransform.m

--- mscripts/LaplaceTransform.m
+++ mscripts/LaplaceTransform.m
@@ -1,14 +1,14 @@
-%Laplace Transform of function f(t) =  16e^-5t
+%%Laplace Transform of function f(t) =  16e^-5t
 
 syms t;
 ft= 16*exp(-5*t);
 Fs = laplace(ft)
 
-%Answer Fs = 16/(s+5)
+%%Answer Fs = 16/(s+5)
 
-%Inverse Laplace Transform of function F(s) = (s^2+2*s+3)/(s^3 + 6*s^2 + 12*s + 8)
+%%Inverse Laplace Transform of function F(s) = (s^2+2*s+3)/(s^3 + 6*s^2 + 12*s + 8)
 syms s t;
 Fs1= (s^2+2*s+3)/(s^3 + 6*s^2 + 12*s + 8);
 ft1= ilaplace(Fs1)
 
-%Answer ft1 = exp(-2*t)-2*t*exp(-2*t)+(3*t^2*exp(-2*t))/2
+%%Answer ft1 = exp(-2*t)-2*t*exp(-2*t)+(3*t^2*exp(-2*t))/2
