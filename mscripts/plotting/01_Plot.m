9285d964887f01f02c2df6b73a34e62b1823f974
Gowtham Ramarajan <84629235+GowthamRam96@users.noreply.github.com>
2021-09-27 11:05:24 +0530

    Create 01_Plot.m

---
+++ mscripts/plotting/01_Plot.m
@@ -0,0 +1,14 @@
+# Plot y = mx + c ; m and c can be assumed to any constant
+
+x = 0: 1: 10;
+m = 5;
+c= 2;
+y = m*x + c;
+plot(x,y);
+title('straight line equation');
+title('Straight line equation');
+xlabel('xaxis')
+ylabel('yaxis')
+grid
+
+#Results
